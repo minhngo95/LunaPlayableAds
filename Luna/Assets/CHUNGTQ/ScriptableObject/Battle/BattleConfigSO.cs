7270143c8a3f1b4aa4e2aab469148b8836e076ee
chungtq <chungtq@horusvn.com>
2024-06-21 14:09:21 +0700

    *Update Scriptable Object

---
+++ Luna/Assets/CHUNGTQ/ScriptableObject/Battle/BattleConfigSO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BattleConfigSO", menuName = "ScriptableObjects/BattleConfig")]
+public class BattleConfigSO : ScriptableObject
+{
+    public FightRounds[] fightRound;
+
+}
+[Serializable]
+public class FightRounds
+{
+    public BotStartConfig[] BotStartConfig;
+}
+[Serializable]
+public class BotStartConfig
+{
+    [Tooltip("Số lượng bot sinh ra")]
+    public int botQuantity;
+    public BotConfigSO BotConfigSO;
+    public float botDelaySpawn;
+}
---
+++ Luna/Assets/CHUNGTQ/ScriptableObject/Bot/BotConfigSO.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BotConfigSO", menuName = "ScriptableObjects/BotConfig")]
+public class BotConfigSO : ScriptableObject
+{
+    public int id;
+    public int health;
+    public int attackDuration;
+    public int damageMinPercent;
+    public int damageMaxPercent;
+    public int damageOnBot;
+    public BotType botType;
+    public GameObject Model;
+    public float moveSpeed;
+    [SerializeField] CarryAttributes[] carryAttributes;
+
+}
+
+[Serializable]
+public class CarryAttributes
+{
+    public BotConfigSO botConfig;
+    public int Quantity;
+}
